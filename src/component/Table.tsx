d72389f6aeff86f6ec31a6175b3f71ea5556fa6c
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-12-20 17:01:58 +0200

    feat: add list virtualization

--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -1,5 +1,6 @@
 import { HeadersConfig, TableRowData } from "../types"
 import { format } from 'date-fns';
+import { TableVirtuoso } from 'react-virtuoso'
 
 type TableProps = {
   headersConfig: HeadersConfig,
@@ -9,16 +10,18 @@ type TableProps = {
 export const Table = ({ headersConfig, data }: TableProps) => {
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 border-separate rtl:text-right dark:text-gray-400" >
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
+      <TableVirtuoso
+        className="!min-h-screen [&_table]:w-full [&_table]:border-separate [&_table]:!border-spacing-0.5 [&_tbody_tr]:border-b  [&_tbody_tr]:odd:dark:bg-gray-900  [&_tbody_tr]:even:dark:bg-gray-800 [&_tbody_tr]:dark:border-gray-700"
+        data={data}
+        fixedHeaderContent={() =>
+          <tr className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             {headerGenerator(headersConfig)}
           </tr>
-        </thead>
-        <tbody>
-          {bodyGenerator(data)}
-        </tbody>
-      </table>
+        }
+        itemContent={(index, row) => (
+          bodyGenerator(row)
+        )}
+      />
     </div>
   )
 }
@@ -33,19 +36,13 @@ const headerGenerator = (headersConfig: HeadersConfig) => {
   })
 }
 
-const bodyGenerator = (data: TableRowData[]) => {
-  return data.map((row, index) => {
+const bodyGenerator = (data: TableRowData) => {
+  return data.map((cell, index) => {
     return (
-      <tr key={index} className="border-b odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700">
-        {row.map((cell, index) => {
-          return (
-            <td scope="row" key={index} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              {typeof cell === 'object' && cell instanceof Date ? format(cell, "yyyy-MM-dd HH:mm") : cell
-              }
-            </td>
-          )
-        })}
-      </tr>
+      <td scope="row" key={index} className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        {typeof cell === 'object' && cell instanceof Date ? format(cell, "yyyy-MM-dd HH:mm") : cell
+        }
+      </td>
     )
   })
 }
